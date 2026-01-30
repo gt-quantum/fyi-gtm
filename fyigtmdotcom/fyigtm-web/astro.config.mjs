7f9918c452ea44c01de354e21bbdb276c062fbaa
gt-quantum <greg@b2bicp.com>
2026-01-30 02:16:15 -0500

    Fix: Remove Cloudflare adapter for static build

--- fyigtmdotcom/fyigtm-web/astro.config.mjs
+++ fyigtmdotcom/fyigtm-web/astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [react()],
-  adapter: cloudflare(),
   output: 'static',
 });
